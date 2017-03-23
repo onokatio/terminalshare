cfd2bc079be95bd675c64db1e3a41a75acb4a271
onokatio <onokatio@gmail.com>
2017-03-23 09:06:59 +0900

    ip

--- send.sh
+++ send.sh
@@ -1,13 +1,13 @@
 #!/bin/zsh
-
+echo $1
 function sendloop(){
 	ls=(`/bin/ls -l sendbuf`)
 	export old=$new
 	export new=$ls[5]
 	diff=$((new-old));
-	tail -c $diff sendbuf | nc localhost 12345
+	tail -c $diff sendbuf | nc $1 12345
 }
-sendloop && while :;do sendloop 2>&1 > /dev/null ;done &
+sendloop $1 && while :;do sendloop $1 2>&1 > /dev/null ;done &
 PID=$!
 script -fq sendbuf
 #kill -9 $PID
